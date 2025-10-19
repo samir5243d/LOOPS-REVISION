34bf04826259af8149792197ad55c58b3383f0ab
samir5243d <samir5243d@gmail.com>
2025-10-19 22:45:39 +0530

    revising loops started

---
+++ src/BASICSofLOOPS/LOOPSbasics.java
@@ -0,0 +1,9 @@
+package BASICSofLOOPS;
+
+public class LOOPSbasics {
+    public static void main(String[] args) {
+        for (int i=1; i<=7; i++){
+            System.out.println("chomu");
+        }
+    }
+}
