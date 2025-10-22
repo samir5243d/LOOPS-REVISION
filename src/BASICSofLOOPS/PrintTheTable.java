945abe09e3a122166d74f7cc0e283846dcb18aa8
samir5243d <samir5243d@gmail.com>
2025-10-23 00:46:12 +0530

    addedd new stuff

--- src/BASICSofLOOPS/PrintTheTable.java
+++ src/BASICSofLOOPS/PrintTheTable.java
@@ -2,6 +2,17 @@ package BASICSofLOOPS;
 
 public class PrintTheTable {
     public static void main(String[] args) {
-        for (int   )
+
+        // ye 172 times chal raha hai not a good code for this problem
+
+//     for (int i=19; i<=190; i++){
+//         if (i%19==0)
+//             System.out.println(i);
+//     }
+        for (int i = 19; i <= 190; i += 19) {
+
+            System.out.println(i);
+        }
     }
 }
+
---
+++ src/BASICSofLOOPS/PrintTheTableOfn.java
@@ -0,0 +1,21 @@
+package BASICSofLOOPS;
+
+import java.util.Scanner;
+public class PrintTheTableOfn {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+
+//        for (int i =1; i<=100; i++)
+//            if (i %n==0)
+//                System.out.println(i);
+
+//        for (int i=n; i<=n *10   ; i+= n){
+//            System.out.println(i);
+
+    }
+}
+
+
